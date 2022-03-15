7958fdac993267954bf517b61920b2cd3a76b475
Harishbhangale <101631562+Harishbhangale@users.noreply.github.com>
2022-03-15 16:46:07 +0530

    Add files via upload

---
+++ Student.java
@@ -0,0 +1,6 @@
+class Student{
+public static void main (String ar[])
+{System.out.println("Name:- Harish Gopal bhangale\n");
+System.out.println("Branch:- Electrical\n");
+System.out.println("College:- Lovely Professional University Punjab\n");
+}}
